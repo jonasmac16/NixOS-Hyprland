47391406bfee25f846240ad6e2ee612476ec01fe
Jonas Mackerodt <jonas.mackerodt@nds.ox.ac.uk>
2026-01-30 12:17:00 +0000

    Update config.nix
    changed v4l lines

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -36,7 +36,12 @@ in {
 
     # This is for OBS Virtual Cam Support
     kernelModules = [ "v4l2loopback" ];
-    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    extraModulePackages = with config.boot.kernelPackages; [
+        v4l2loopback
+    ];
+    extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
 
     initrd = {
       availableKernelModules = [
