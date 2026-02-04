dbe8880ef5734fb181e7b1eabb2a45353dd4e6c4
jonasmac16 <jonas.mackerodt@nds.ox.ac.uk>
2026-02-04 16:43:16 +0000

    workstation host

---
+++ hosts/workstation/variables.nix
@@ -0,0 +1,18 @@
+# 💫 https://github.com/JaKooLit 💫 #
+# Variables
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "jonasmac16";
+  gitEmail = "jonas.mackerodt@nds.ox.ac.uk";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
