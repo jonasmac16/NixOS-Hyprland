dda2ba570aa3ba7dcb9ef969fdf1e3675e61edda
jonasmac16 <jonas.mackerodt@nds.ox.ac.uk>
2026-02-10 17:17:04 +0000

    scifox user css

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -15,70 +15,106 @@ let
 
   # ~/.mozilla/firefox/PROFILE_NAME/prefs.js | user.js
   settings = {
-    "app.normandy.first_run" = false;
-    "app.shield.optoutstudies.enabled" = false;
+    # "app.normandy.first_run" = false;
+    # "app.shield.optoutstudies.enabled" = false;
+    # 
+    # # disable updates (pretty pointless with nix)
+    # "app.update.channel" = "default";
+    # 
+    # "browser.contentblocking.category" = "standard"; # "strict"
+    # "browser.ctrlTab.recentlyUsedOrder" = false;
+    # 
+    # "browser.download.useDownloadDir" = false;
+    # "browser.download.viewableInternally.typeWasRegistered.svg" = true;
+    # "browser.download.viewableInternally.typeWasRegistered.webp" = true;
+    # "browser.download.viewableInternally.typeWasRegistered.xml" = true;
+    # 
+    # "browser.link.open_newwindow" = true;
+    # 
+    # "browser.search.region" = "PL";
+    # "browser.search.widget.inNavBar" = true;
+    # 
+    # # "browser.shell.checkDefaultBrowser" = false;
+    # "browser.startup.homepage" = "";
+    # "browser.tabs.loadInBackground" = true;
+    # "browser.urlbar.placeholderName" = "DuckDuckGo";
+    # # "browser.urlbar.showSearchSuggestionsFirst" = false;
+    # 
+    # # disable all the annoying quick actions
+    # "browser.urlbar.quickactions.enabled" = false;
+    # "browser.urlbar.quickactions.showPrefs" = false;
+    # "browser.urlbar.shortcuts.quickactions" = false;
+    # "browser.urlbar.suggest.quickactions" = false;
+    # 
+    # "distribution.searchplugins.defaultLocale" = "en-US";
+    # 
+    # "doh-rollout.balrog-migration-done" = true;
+    # "doh-rollout.doneFirstRun" = true;
+    # 
+    # "dom.forms.autocomplete.formautofill" = false;
+    # 
+    # "general.autoScroll" = true;
+    # "general.useragent.locale" = "en-GB";
+    # 
+    # # "extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
+    # 
+    # # "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
+    # "extensions.update.enabled" = false;
+    # "extensions.webcompat.enable_picture_in_picture_overrides" = true;
+    # "extensions.webcompat.enable_shims" = true;
+    # "extensions.webcompat.perform_injections" = true;
+    # "extensions.webcompat.perform_ua_overrides" = true;
+    # 
+    # "print.print_footerleft" = "";
+    # "print.print_footerright" = "";
+    # "print.print_headerleft" = "";
+    # "print.print_headerright" = "";
+    # 
+    # "privacy.donottrackheader.enabled" = true;
+    # 
+    # # Yubikey
+    # "security.webauth.u2f" = true;
+    # "security.webauth.webauthn" = true;
+    # "security.webauth.webauthn_enable_softtoken" = true;
+    # "security.webauth.webauthn_enable_usbtoken" = true;
 
-    # disable updates (pretty pointless with nix)
-    "app.update.channel" = "default";
-
-    "browser.contentblocking.category" = "standard"; # "strict"
-    "browser.ctrlTab.recentlyUsedOrder" = false;
-
-    "browser.download.useDownloadDir" = false;
-    "browser.download.viewableInternally.typeWasRegistered.svg" = true;
-    "browser.download.viewableInternally.typeWasRegistered.webp" = true;
-    "browser.download.viewableInternally.typeWasRegistered.xml" = true;
-
-    "browser.link.open_newwindow" = true;
-
-    "browser.search.region" = "PL";
-    "browser.search.widget.inNavBar" = true;
+    # "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
     "browser.shell.checkDefaultBrowser" = false;
-    "browser.startup.homepage" = "https://nixos.org";
-    "browser.tabs.loadInBackground" = true;
-    "browser.urlbar.placeholderName" = "DuckDuckGo";
+    "browser.shell.defaultBrowserCheckCount" = 1;
+    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar" = false;
+    "widget.use-xdg-desktop-portal.file-picker" = 1;
+    "widget.use-xdg-desktop-portal.mime-handler" = 1;
+    "browser.search.suggest.enabled" = false;
+    "browser.search.suggest.enabled.private" = false;
+    "browser.urlbar.suggest.searches" = false;
     "browser.urlbar.showSearchSuggestionsFirst" = false;
-
-    # disable all the annoying quick actions
-    "browser.urlbar.quickactions.enabled" = false;
-    "browser.urlbar.quickactions.showPrefs" = false;
-    "browser.urlbar.shortcuts.quickactions" = false;
-    "browser.urlbar.suggest.quickactions" = false;
-
-    "distribution.searchplugins.defaultLocale" = "en-US";
-
-    "doh-rollout.balrog-migration-done" = true;
-    "doh-rollout.doneFirstRun" = true;
-
-    "dom.forms.autocomplete.formautofill" = false;
-
-    "general.autoScroll" = true;
-    "general.useragent.locale" = "en-US";
-
-    "extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-
-    "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-    "extensions.update.enabled" = false;
-    "extensions.webcompat.enable_picture_in_picture_overrides" = true;
-    "extensions.webcompat.enable_shims" = true;
-    "extensions.webcompat.perform_injections" = true;
-    "extensions.webcompat.perform_ua_overrides" = true;
-
-    "print.print_footerleft" = "";
-    "print.print_footerright" = "";
-    "print.print_headerleft" = "";
-    "print.print_headerright" = "";
-
-    "privacy.donottrackheader.enabled" = true;
-
-    # Yubikey
-    "security.webauth.u2f" = true;
-    "security.webauth.webauthn" = true;
-    "security.webauth.webauthn_enable_softtoken" = true;
-    "security.webauth.webauthn_enable_usbtoken" = true;
-
+    "browser.sessionstore.enabled" = true;
+    "browser.sessionstore.resume_from_crash" = true;
+    "browser.sessionstore.resume_session_once" = true;
     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    "browser.tabs.drawInTitlebar" = true;
+    "svg.context-properties.content.enabled" = true;
+    "general.smoothScroll" = true;
+    "uc.tweak.hide-tabs-bar" = true;
+    "uc.tweak.hide-forward-button" = true;
+    "uc.tweak.rounded-corners" = true;
+    "uc.tweak.floating-tabs" = true;
+    "layout.css.color-mix.enabled" = true;
+    "layout.css.light-dark.enabled" = true;
+    "layout.css.has-selector.enabled" = true;
+    "media.ffmpeg.vaapi.enabled" = true;
+    "media.rdd-vpx.enabled" = true;
+    "browser.tabs.tabmanager.enabled" = false;
+    "full-screen-api.ignore-widgets" = false;
+    "browser.urlbar.suggest.engines" = false;
+    "browser.urlbar.suggest.openpage" = false;
+    "browser.urlbar.suggest.bookmark" = false;
+    "browser.urlbar.suggest.addons" = false;
+    "browser.urlbar.suggest.pocket" = false;
+    "browser.urlbar.suggest.topsites" = false;
+
   };
 
   demoSettings = {
@@ -154,9 +190,36 @@ in
   programs.firefox = {
     enable = true;
 
+    package = pkgs.wrapFirefox pkgs.firefox-beta-unwrapped {
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = false;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OfferToSaveLoginsDefault = false;
+        PasswordManagerEnabled = false;
+        FirefoxHome = {
+          Search = true;
+          Pocket = false;
+          Snippets = false;
+          TopSites = false;
+          Highlights = false;
+        };
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+      };
+    };
+
     profiles = {
       default = {
         id = 0;
+        name = "work";
+        isDefault = true;
         extensions.packages = with inputs.rycee-nurpkgs.packages.${pkgs.system}; [
           proton-pass
           darkreader
@@ -166,8 +229,11 @@ in
           privacy-badger
           ublock-origin
           tridactyl
+          sidebery
+          adaptive-tab-bar-colour
         ];
-        inherit settings userChrome;
+        userChrome = (builtins.readFile ./userChrome.css);
+        userContent = (builtins.readFile ./userContent.css);
       };
     };
   };
